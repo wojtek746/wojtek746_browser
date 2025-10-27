743903542dc135c754f2b6e319b05c50c231b622
wojtek746 <wojtek7462@gmail.com>
2025-10-27 17:39:11 +0100

    naprawa błędów z menu

--- ContextMenu.py
+++ ContextMenu.py
@@ -1,28 +1,57 @@
-from PyQt6.QtWidgets import QMenu, QFileDialog
+from PyQt6.QtCore import QUrl
+from PyQt6.QtWidgets import QMenu, QFileDialog, QApplication
 import requests
 
 class CustomContextMenu:
     def _context_menu(self, tab, pos):
-        data = tab.view.page().contextMenuData()
-        menu = QMenu(tab)
+        x = int(pos.x())
+        y = int(pos.y())
+        js = f"""
+        (function(){{
+          try {{
+            var el = document.elementFromPoint({x},{y});
+            if (!el) return {{link:'', img:''}};
+            var a = el.closest ? el.closest('a') : (function(){{
+                var p=el; while(p && p.tagName!=='A') p=p.parentElement; return p;
+            }})();
+            var link = (a && a.href) ? a.href : '';
+            var img = '';
+            if (el.tagName === 'IMG') img = el.src || '';
+            else {{
+              var inside = el.querySelector && el.querySelector('img');
+              if (inside) img = inside.src || '';
+            }}
+            return {{link: link, img: img}};
+          }} catch(e) {{ return {{link:'', img:''}}; }}
+        }})();
+        """
 
-        #dodać więcej opcji (kopiuj link)
-        if data.mediaType() == data.MediaTypeImage and data.mediaUrl().isValid():
-            open_img_tab = menu.addAction("Open image in new tab")
-            open_img_tab.triggered.connect(lambda: self._open_tab(data.mediaUrl(), tab))
-            save_img_as = menu.addAction("Save image as...")
-            save_img_as.triggered.connect(lambda: self._save_image(data.mediaUrl(), tab))
-        if data.linkUrl().isValid():
-            open_link_tab = menu.addAction("Open link in new tab")
-            open_link_tab.triggered.connect(lambda: self._open_tab(data.linkUrl(), tab))
+        def cb(res):
+            link = ""
+            img = ""
+            try:
+                if isinstance(res, dict):
+                    link = res.get("link", "") or ""
+                    img = res.get("img", "") or ""
+            except Exception:
+                pass
 
-        menu.exec_(tab.view.mapToGlobal(pos))
+            menu = QMenu(tab)
+            if img:
+                menu.addAction("Open image in new tab", lambda: self._open_tab(img, tab))
+                menu.addAction("Save image as...", lambda: self._save_image(img, tab))
+            if link:
+                menu.addAction("Open link in new tab", lambda: self._open_tab(link, tab))
+                menu.addAction("Copy link address", lambda: QApplication.clipboard().setText(link))
+            menu.exec(tab.view.mapToGlobal(pos))
+
+        tab.view.page().runJavaScript(js, cb)
 
     def _open_tab(self, url, tab):
         win = tab.window()
         if hasattr(win, "add_tab"):
             prev_index = win.tabs.currentIndex()
-            win.add_tab(url.toString(), True)
+            win.add_tab(url, True)
             c = win.tabs.count()
             win.move_tab(c - 1, win.tabs.indexOf(tab) + 1)
             if win.tabs.currentIndex() != prev_index:
@@ -32,7 +61,7 @@ class CustomContextMenu:
     def _save_image(self, url, tab):
         QFileDialog.getSaveFileName()
         path, _ = QFileDialog.getSaveFileName(tab, "Save image as", "", "Images (*.png *.jpg *.jpeg *.webp *.gif)")
-        r = requests.get(url.toString(), timeout=10)
+        r = requests.get(url, timeout=10)
         if r.status_code == 200:
             with open(path, "wb") as f:
                 f.write(r.content)
